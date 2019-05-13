be3a9f8e4bbb2c806492333659d140f4ca89e86d
Bernard William Dave <0970563@decinfo.cvm>
2019-05-13 12:40:07 -0400

    exercice 2 final

--- exercices/exercice2.sh
+++ exercices/exercice2.sh
@@ -1,42 +1,41 @@
 #!/bin/bash
 #Faire un menu pour accéder au dictionnaire "d"
 #Tant que l'usager veut chercher un mot l(ettre o)
-	#cherche un mot (le code de l'exercice 1)
+        #cherche un mot (le code de l'exercice 1)
 
 cherche=o
 choixInvalide="o"
 while test $choixInvalide = "o"; do
-	#-------------------------------------------
-	clear
-	echo "Chercheur d'existance vocabularial"
-	echo "Accéder au dictionaire:	d"
-	echo "Quitter:					q"
-	read -p "Reponse: " reponse 
-	#-------------------------------------------
-	#-------------------------------------------
-	if test $reponse = "d";then
-		while test $cherche = o; do
-			clear
-			read -p "Entrez un mot : " mot
+        #-------------------------------------------
+        clear
+        echo "Chercheur d'existance vocabularial"
+        echo "Accéder au dictionaire:   d"
+        echo "Quitter:          q"
+        read -p "Reponse: " reponse
+        #-------------------------------------------
+        #-------------------------------------------
+        if test $reponse = "d";then
+                while test $cherche = o; do
+                        clear
 
-			result=`wget -qO - http://dictionary.objectif8.com/exists.php?word=$mot` # commande "wget" ne fonctione pas sur windows 
-			seulement sur linux
+                        ./exercice1.sh
 
-			if test $result = "1";then
-					echo "Ce mot existe."
-			else
-					echo "Ce mot n'existe pas."
-			fi
-			
-			read -p "Chercher un mot? (o = oui): " cherche
-		done
-		
-		echo "Merci, au revoir!"
-		choixInvalide="n"
-	elif test $reponse = "q"; then
-		echo "Merci, au revoir!"
-		choixInvalide="n"
-	else
-		choixInvalide="o"
-	#--------------------------------------------
+                        read -p "Chercher un mot? (o = oui): " cherche
+                done
+
+                clear
+                echo "Merci, au revoir!"
+                sleep 3
+                clear
+                choixInvalide="n"
+        elif test $reponse = "q"; then
+                clear
+                echo "Merci, au revoir!"
+                sleep 3
+                clear
+                choixInvalide="n"
+        else
+                choixInvalide="o"
+        fi
+        #--------------------------------------------
 done
